0d56e51dbb5d7870efbc7e59834173dcb066b517
Sergey Kurskiy <kurskiisergey@mail.ru>
2022-02-24 14:23:53 +0300

    add save to json

--- lesson1/github_api.py
+++ lesson1/github_api.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from pprint import pprint
 
 
@@ -44,20 +45,34 @@ class GitHubApi:
 
         return result
 
-    def show_user_repos(self, username=None, auth=False):
+    @staticmethod
+    def _save_info(name, info):
+        with open(name, 'w', encoding='utf-8') as save_file:
+            try:
+                json.dump(info, save_file)
+            except json.JSONDecodeError:
+                save_file.write(info)
+
+    def show_user_repos(self, username=None, auth=False, save=True):
         user_repos = self._get_repos_list(username=username, auth=auth)
-        print(f"Список репозиториев пользователя {self.username if username is None else username}")
+        user = self.username if username is None else username
+        print(f"Список репозиториев пользователя {user}")
+        if save:
+            GitHubApi._save_info(f"{user}_repos.json", user_repos)
         pprint(user_repos)
 
-    def show_user_info(self, username=None, auth=False):
+    def show_user_info(self, username=None, auth=False, save=True):
         info = self._get_user(username=username, auth=auth)
-        print(f"Информация о пользователе {info.get('login')}")
+        user = info.get('login')
+        print(f"Информация о пользователе {user}")
+        if save:
+            GitHubApi._save_info(f"{user}.json", info)
         pprint(info)
 
 
 if __name__ == "__main__":
     login = "KurskiySergey"
-    access_token = "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"
+    access_token = "access_token"
     api = GitHubApi(username=login, token=access_token)
-    api.show_user_info(auth=True)
-    api.show_user_repos(auth=True)
+    api.show_user_info(auth=False, save=True)
+    api.show_user_repos(auth=False, save=True)
